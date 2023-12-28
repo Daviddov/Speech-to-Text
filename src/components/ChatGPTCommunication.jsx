3bc3ceea959d3fa7566716eea0cc8fa80da453ef
Dovbar <baror2009@gmail.com>
2023-12-28 13:15:28 +0200

    reorder component

--- src/components/ChatGPTCommunication.jsx
+++ src/components/ChatGPTCommunication.jsx
@@ -2,8 +2,6 @@
 
 import React, { Component } from 'react';
 import axios from 'axios';
-import SpeakTextComponent from './SpeakTextComponent';
-import OpenAITTSComponent from './OpenAITTSComponent';
 
 class ChatGPTCommunication extends Component {
   constructor(props) {
@@ -12,30 +10,16 @@ class ChatGPTCommunication extends Component {
       loading: false,
       error: null,
       userMessage: '',
-      aiResponse: '',
     };
-  }
-
-  
-  async componentDidUpdate(prevProps, prevState) {
-    const { aiResponse } = this.state;
-
-    // Check if aiResponse has changed
-    if (aiResponse !== prevState.aiResponse) {
-      // Start the audio stream only if the response is non-empty
-      if (aiResponse.trim() !== '') {
-        this.openAITTSComponentRef.streamAudio();
-      }
-    }
   }
 
   async sendToChatGPT(transcription) {
     try {
-      const { apiKey } = this.props;
+      const { apiKey, onAiResponse } = this.props;
       const apiUrl = 'https://api.openai.com/v1/chat/completions';
-  
+
       this.setState({ loading: true, error: null });
-  
+
       const response = await axios.post(
         apiUrl,
         {
@@ -46,19 +30,12 @@ class ChatGPTCommunication extends Component {
               content: `I want you to act as a spoken English teacher and improver. 
               you will reply to me in English to practice my spoken English.
               limiting the reply to 40 words. ask me a question in your reply. 
-              you could ask me a question first. `,
-               
-
-                 //  I want you to correct my grammar mistakes.
-                  // after that put "|" and
-                  // Give 3 suggestions for sentences for me to answer you. 
-                  // and reply If the sentence is correct, If not, provide the correct sentence.
+              you could ask me a question first.`,
             },
             { role: 'user', content: transcription },
           ],
-          // Add temperature and max tokens parameters for a more diverse and concise response
-          temperature: 0.7, // Adjust the temperature (higher values for more randomness, lower for more determinism)
-          max_tokens: 150, // Limit the response to a certain number of tokens
+          temperature: 0.7,
+          max_tokens: 150,
         },
         {
           headers: {
@@ -69,8 +46,10 @@ class ChatGPTCommunication extends Component {
       );
 
       const chatGPTResponse = response.data.choices[0].message.content;
-      this.setState({ aiResponse: chatGPTResponse, loading: false });
+      this.setState({ loading: false });
 
+      // Update the aiResponse in the parent component
+      onAiResponse(chatGPTResponse);
     } catch (error) {
       console.error(
         'Error sending transcription to ChatGPT:',
@@ -81,24 +60,19 @@ class ChatGPTCommunication extends Component {
   }
 
   render() {
-    const { loading, error, userMessage, aiResponse } = this.state;
-    const { apiKey } = this.props;
+    const { loading, error, userMessage } = this.state;
 
     return (
       <div>
         <div>
-            <strong>User:</strong> {userMessage}
+          <strong>User:</strong> {userMessage}
           <div>
             <strong>AI English Teacher:</strong>
-            <br /> 
-             {aiResponse}
+            <br />
+            {/* Display aiResponse received as a prop */}
+            {this.props.aiResponse}
           </div>
         </div>
-        <OpenAITTSComponent
-          ref={(ref) => (this.openAITTSComponentRef = ref)}
-          apiKey={apiKey}
-          input={aiResponse} // Use userMessage instead of aiResponse
-        />
       </div>
     );
   }
--- src/components/SpeechToTextWithChatGPT.jsx
+++ src/components/SpeechToTextWithChatGPT.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import SpeechToText from './SpeechToText';
 import ChatGPTCommunication from './ChatGPTCommunication';
-
+import OpenAITTSComponent from './OpenAITTSComponent';
 
 class SpeechToTextWithChatGPT extends Component {
   constructor(props) {
@@ -10,14 +10,33 @@ class SpeechToTextWithChatGPT extends Component {
     this.state = {
       apiKey: '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',
       transcription: '',
+      aiResponse: '',
     };
 
     this.handleSpeechRecognitionEnd = this.handleSpeechRecognitionEnd.bind(this);
+    this.handleAiResponse = this.handleAiResponse.bind(this);
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    const { aiResponse } = this.state;
+
+    // Check if aiResponse has changed
+    if (aiResponse !== prevState.aiResponse) {
+      // Start the audio stream only if the response is non-empty
+      if (aiResponse.trim() !== '') {
+        this.openAITTSComponentRef.streamAudio();
+      }
+    }
   }
 
   handleSpeechRecognitionEnd(transcription) {
     this.setState({ transcription });
     this.sendToChatGPT(transcription);
+  }
+
+  handleAiResponse(response) {
+    // Update the aiResponse in the state
+    this.setState({ aiResponse: response });
   }
 
   async sendToChatGPT(transcription) {
@@ -26,13 +45,23 @@ class SpeechToTextWithChatGPT extends Component {
   }
 
   render() {
-    const { apiKey, transcription } = this.state;
+    const { apiKey, transcription, aiResponse } = this.state;
 
     return (
       <div>
         <SpeechToText onSpeechRecognitionEnd={this.handleSpeechRecognitionEnd} />
-        <ChatGPTCommunication apiKey={apiKey} userMessage={transcription} ref={(ref) => (this.chatGPTCommunicationRef = ref)} />
-
+        <ChatGPTCommunication
+          apiKey={apiKey}
+          userMessage={transcription}
+          aiResponse={aiResponse} // Pass aiResponse as a prop
+          onAiResponse={this.handleAiResponse} // Pass the handler function
+          ref={(ref) => (this.chatGPTCommunicationRef = ref)}
+        />
+        <OpenAITTSComponent
+          ref={(ref) => (this.openAITTSComponentRef = ref)}
+          apiKey={apiKey}
+          input={aiResponse} // Use transcription instead of aiResponse
+        />
       </div>
     );
   }
