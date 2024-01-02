42f0f847f51891c82ab6943458b845086648719f
Dovbar <baror2009@gmail.com>
2024-01-02 15:27:26 +0200

    add google login

--- src/index.js
+++ src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId="649887607827-bdmm29gkdrd8dft2rbdsiehe7h280uc4.apps.googleusercontent.com">
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </GoogleOAuthProvider>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
